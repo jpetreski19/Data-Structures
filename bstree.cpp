b9fd6589b21d7d975e3ca17e9117672497455a9f
Jovan <jovan.petreski@student.manchester.ac.uk>
2021-04-06 20:31:25 +0100

    Fix indentation

--- bstree.cpp
+++ bstree.cpp
@@ -5,86 +5,88 @@ using namespace std;
 
 class node {
 public:
-	int value;
-	node* left_child;
-	node* right_child;
+  int value;
+  node* left_child;
+  node* right_child;
 
-	node (int value) {
-		this->value = value;
-		left_child = NULL;
-		right_child = NULL;
-	}
-
-
-	void insert(int x) {
-		if (x <= this->value) {
-			if (this->left_child == NULL) {
-				this->left_child = new node(x);
-			} else {
-				this->left_child->insert(x);
-			}
-		} else {
-			if (this->right_child == NULL) {
-				this->right_child = new node(x);
-			} else {
-				this->right_child->insert(x);
-			}
-		}
-	}
+  node (int value) {
+    this->value = value;
+    left_child = NULL;
+    right_child = NULL;
+  }
 
 
-	bool find(int x) {
-		if (this == NULL) {
-			return false;
-		}
-		else if (this->value == x) {
-			return true;
-		}
+  void insert(int x) {
+    if (x <= this->value) {
+      if (this->left_child == NULL) {
+        this->left_child = new node(x);
+      } else {
+        this->left_child->insert(x);
+      }
+    } else {
+      if (this->right_child == NULL) {
+        this->right_child = new node(x);
+      } else {
+        this->right_child->insert(x);
+      }
+    }
+  }
 
-		if (x < this->value) {
-			return this->left_child->find(x);
-		} else {
-			return this->right_child->find(x);
-		}
-	}
+
+  bool find(int x) {
+    if (this == NULL) {
+      return false;
+    }
+    else if (this->value == x) {
+      return true;
+    }
+
+    if (x < this->value) {
+      return this->left_child->find(x);
+    } else {
+      return this->right_child->find(x);
+    }
+  }
 
 
-	void inorder() {
-		if (this == NULL) {
-			return;
-		}
+  void inorder() {
+    if (this == NULL) {
+      return;
+    }
 
-		this->left_child->inorder();
-		cout << this->value << endl;
-		this->right_child->inorder();
-	}
+    this->left_child->inorder();
+    cout << this->value << endl;
+    this->right_child->inorder();
+  }
 };
 
 int main() {
 
-	int n;
-	cin >> n;
+  int n;
+  cin >> n;
 
-	int a[n + 1];
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-	}
+  int a[n + 1];
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
 
-	node* root = new node(a[0]);
-	for (int i = 1; i < n; i++) {
-		root->insert(a[i]);
-	}
+  node* root = new node(a[0]);
+  for (int i = 1; i < n; i++) {
+    root->insert(a[i]);
+  }
 
-	// root->inorder();
+  // root->inorder();
 
-	int q;
-	int e;
-	cin >> q;
+  int q;
+  int e;
+  cin >> q;
 
-	for (int i = 0; i < q; i++) {
-		cin >> e;
-		cout << root->find(e) << endl;
-	}
+  for (int i = 0; i < q; i++) {
+    cin >> e;
+    cout << root->find(e) << endl;
+  }
 
-	return 0;
+  delete root;
+
+  return 0;
 }
